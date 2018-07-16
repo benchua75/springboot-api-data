94a6301f726829de48501c557fd0c858b6319131
lbchua <lbchua@ncs-300114ms01.ncs.corp.int-ads>
2018-07-16 16:42:48 +0800

    initial commit

---
+++ src/main/java/org/test/SprintbootApiDataApplication.java
@@ -0,0 +1,12 @@
+package org.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootApiDataApplication.class, args);
+	}
+}
